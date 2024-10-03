d2fcc801067d3ba34c2d74275705fd412b1a3de6
unknown <bbdostoevskij@gmail.com>
2024-10-03 23:46:27 +0300

    work on student_validator.c and some previous forgotten files

--- structs/student_validator.c
+++ structs/student_validator.c
@@ -12,7 +12,22 @@ typedef struct
 {
     Date date_of_birth;
     char sex;
-} StudentDate;
+} PersonDate;
+
+int validate_input(PersonDate card)
+{
+    return 
+    !(
+        card.date_of_birth.year < 1901 || 
+        card.date_of_birth.day > 31 || 
+        card.date_of_birth.month > 12 || 
+        card.date_of_birth.day < 1 || 
+        card.date_of_birth.month < 1
+    ) || (
+        card.sex != 'M' && 
+        card.sex != 'F'
+    );
+}
 
 int calculate_age(int birthYear) 
 {
@@ -22,71 +37,229 @@ int calculate_age(int birthYear)
     return this_year - birthYear;
 }
 
-int checkCard(StudentDate card) 
+int check_card(PersonDate card) 
 {
     int birth = card.date_of_birth.year;
     int age = calculate_age(card.date_of_birth.year);
     char sex = card.sex;
-
-    return birth < 1901 || age < 18 || age > 50 || sex == 'M' || sex == 'm';
+    return validate_input(card) && age >= 18 && age <= 55;
 }
 
-StudentDate feedBack(StudentDate card)
+void feed_back(PersonDate card)
 {
     int birth = card.date_of_birth.year;
-    int age =  calculate_age(card.date_of_birth.year);
-    if (checkCard(card))
+    int age = calculate_age(card.date_of_birth.year);
+    if (check_card(card))
     {
         printf("Sorry, not for you %d", age);
     } 
     else 
     {
-        printf("Nice... \nAge is: %d", age);
+        printf("Nice... Age is: %d", age);
     }
+}
+
+int can_engage_in_sexual_activity(PersonDate card1, PersonDate card2)
+{
+    int age_difference = card1.date_of_birth.year - card2.date_of_birth.year;
+    return (age_difference < 30 || age_difference > -30) && card1.sex != card2.sex;
+}
+
+char to_uppercase(char letter) 
+{
+    if (letter == 'f')
+    {
+        return 'F';
+    } 
+    else if (letter == 'm')
+    {
+        return 'M';
+    }
+    else {
+        return letter;
+    } 
 }
 
 int main()
 {
-    
-
     int birthDay;
     int birthMonth;
     int birthYear;
     char sex;
 
-    printf("\nYours sex (M if male and F if female) is ");
-    scanf("%c", &sex);
+    int studentBirthDay;
+    int studentBirthMonth;
+    int studentBirthYear;
+    char studentSex;
 
-    printf("\nDay of birth is ");
-    scanf("%d", &birthDay);
+    int teacherBirthDay;
+    int teacherBirthMonth;
+    int teacherBirthYear;
+    char teacherSex;
 
-    printf("\nMonth of birth is ");
-    scanf("%d", &birthMonth);
+    int hamaliyBirthDay;
+    int hamaliyBirthMonth;
+    int hamaliyBirthYear;
+    char hamaliySex;
+    
+    printf("Yours sex (M if male and F if female) is ");
+    scanf(" %c", &sex);
 
-    printf("\nYear of birth is ");
+    printf("Year of birth is ");
     scanf("%d", &birthYear);
 
-    StudentDate card = {
+    printf("Day of birth is ");
+    scanf("%d", &birthDay);
+
+    printf("Month of birth is ");
+    scanf("%d", &birthMonth);
+
+    printf("Yours sex (M if male and F if female) is ");
+    scanf(" %c", &studentSex);
+
+    printf("Year of birth is ");
+    scanf("%d", &studentBirthYear);
+
+    printf("Day of birth is ");
+    scanf("%d", &studentBirthDay);
+
+    printf("Month of birth is ");
+    scanf("%d", &studentBirthMonth);
+
+    printf("Teachers sex (M if male and F if female) is ");
+    scanf(" %c", &teacherSex);
+
+    printf("Teachers year of birth is ");
+    scanf("%d", &teacherBirthYear);
+
+    printf("Teachers day of birth is ");
+    scanf("%d", &teacherBirthDay);
+
+    printf("Teachers month of birth is ");
+    scanf("%d", &teacherBirthMonth);
+
+    printf("Hamaliy sex (M if male and F if female) is ");
+    scanf(" %c", &hamaliySex);
+
+    printf("Hamaliy year of birth is ");
+    scanf("%d", &hamaliyBirthYear);
+
+    printf("Hamaliy day of birth is ");
+    scanf("%d", &hamaliyBirthDay);
+
+    printf("Hamaliy month of birth is ");
+    scanf("%d", &hamaliyBirthMonth);
+
+    PersonDate card = {
         .date_of_birth = {
             .day = birthDay, 
             .month = birthMonth, 
             .year = birthYear
         },
-        .sex = sex,
+        .sex = to_uppercase(sex),
     };
 
-    if (birthYear < 1901 || birthDay > 31 || birthMonth > 12 || birthDay < 1 || birthMonth < 1)
+    PersonDate studentCard = {
+        .date_of_birth = {
+            .day = studentBirthDay, 
+            .month = studentBirthMonth, 
+            .year = studentBirthYear
+        },
+        .sex = to_uppercase(studentSex),
+    };
+
+    PersonDate teacherCard = {
+        .date_of_birth = {
+            .day = teacherBirthDay, 
+            .month = teacherBirthMonth, 
+            .year = teacherBirthYear
+        },
+        .sex = to_uppercase(teacherSex),
+    };
+
+    PersonDate hamaliyCard = {
+        .date_of_birth = {
+            .day = hamaliyBirthDay, 
+            .month = hamaliyBirthMonth, 
+            .year = hamaliyBirthYear
+        },
+        .sex = to_uppercase(hamaliySex),
+    };
+
+    if (!validate_input(card) || 
+        !validate_input(studentCard) ||
+        !validate_input(teacherCard) || 
+        !validate_input(hamaliyCard)) 
     {
-        printf("Invalid date");
+        printf("invalid input: bad boy");
+        return 1;
     }
-    else if(sex == 'm' || sex == 'M' || sex == 'f' || sex == 'F')
+
+    if (can_engage_in_sexual_activity(card, studentCard))
     {
-        feedBack(card);
+        printf("I can fuck the student.");
     } 
     else
     {
-        printf("Invalid gender");
+        printf("The student is not for me.");
+    }
+
+    if (can_engage_in_sexual_activity(card, teacherCard))
+    {
+        printf("I can fuck the teacher.");
+    } 
+    else
+    {
+        printf("The teacher is not for me.");
+    }
+
+    if (can_engage_in_sexual_activity(studentCard, teacherCard))
+    {
+        printf("The student can fuck the teacher.");
+    } 
+    else
+    {
+        printf("The teacher is not for student.");
+    }
+
+    if (can_engage_in_sexual_activity(teacherCard, hamaliyCard))
+    {
+        printf("Hamaliy can fuck the teacher.");
+    } 
+    else
+    {
+        printf("The Hamaliy is not for teacher.");
     }
 
     return 0;
 }
+
+/*
+
+Rename StudentCard => PersonCard;
+Leave already implemented algorithm and extend it by 
+    adding teacher_card object of type PersonCard
+Ask user to input sex and date of birth for teacher 
+Fill the teacher_card object
+Likewise create and fill hamaliy object and implement the following functionality
+After user inputs all the stuff print 3 conclusions: 
+    1; You can have sex with student or not.
+    2; Teacher can have sex with student or not.
+    3; Hamaliy can have sex with teacher or not.
+Two people can have sex under the same requirements.
+Always print descriptive messages so that we can always understand who is fucking.
+Pay special attencion to avoiding code duplication.
+
+DONE
+
+NEW HOMEWORK
+    1. push current version of student_validator.c to getHam;
+    2. copy the contents of the file to another file;
+    3. modify the copy so that objects of structs are passed to functions via pointers;
+    4. implement modifiers (setters) corresponding to all the implemented selectors (getters) in the almost_oop.c 
+    5. ogo ti izvraschenets;
+
+
+
+
+*/
