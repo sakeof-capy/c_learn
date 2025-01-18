b798983df45612580c193100fca96185bb8fbe25
unknown <bbdostoevskij@gmail.com>
2025-01-18 21:37:56 +0200

    arr[found_index] = arr[*arr_size -1];

--- 3_arrays/src/error_code_processing.c
+++ 3_arrays/src/error_code_processing.c
@@ -4,29 +4,29 @@
 
 void error_code_processing(int* arr, int element, int* arr_size, int error_code)
 {
-if (error_code == SUCCESS)
-{
-    printf("Element %d deleted successfully!\n", element);
-    if (*arr_size < 20)
+    if (error_code == SUCCESS)
     {
-        printf("New array: \n");
-        for (int i = 0; i < *arr_size; i++)
+        printf("Element %d deleted successfully!\n", element);
+        if (*arr_size < 20)
         {
-            printf("%d ", arr[i]);
+            printf("New array: \n");
+            for (int i = 0; i < *arr_size; i++)
+            {
+                printf("%d ", arr[i]);
+            }
         }
+        printf("\n");
     }
-    printf("\n");
-}
-else if (error_code == ELEMENT_NOT_FOUND)
-{
-    printf("Element not found\n");
-}
-else if (error_code == NULL_POINTER_ERROR)
-{
-    printf("NULL\n");
-}
-else 
-{
-    printf("Unknown error happened\n");
-}
+    else if (error_code == ELEMENT_NOT_FOUND)
+    {
+        printf("Element not found\n");
+    }
+    else if (error_code == NULL_POINTER_ERROR)
+    {
+        printf("NULL\n");
+    }
+    else 
+    {
+        printf("Unknown error happened\n");
+    }
 }
